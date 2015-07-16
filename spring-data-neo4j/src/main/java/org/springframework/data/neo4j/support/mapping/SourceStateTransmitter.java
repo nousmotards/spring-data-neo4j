6f7fbd3faeea95b702d73821bc8da57dfaed9a02
Romaric P <evoxmusic@gmail.com>
2015-07-16 14:33:23 +0200

    fix: lost properties on Node due to not invoking the removed block

--- spring-data-neo4j/src/main/java/org/springframework/data/neo4j/support/mapping/SourceStateTransmitter.java
+++ spring-data-neo4j/src/main/java/org/springframework/data/neo4j/support/mapping/SourceStateTransmitter.java
@@ -16,7 +16,6 @@
 package org.springframework.data.neo4j.support.mapping;
 
 import org.neo4j.graphdb.*;
-import org.springframework.data.neo4j.annotation.NodeEntity;
 import org.springframework.data.mapping.Association;
 import org.springframework.data.mapping.AssociationHandler;
 import org.springframework.data.mapping.PersistentPropertyAccessor;
@@ -106,8 +105,11 @@ public class SourceStateTransmitter<S extends PropertyContainer> {
                     setEntityStateValue(property, entityState, wrapper, property.getMappingPolicy());
                 }
             });
-            if (target instanceof UpdateableState && entityStateObject instanceof NodeEntity) {
-                ((UpdateableState) target).flush();
+            if (target instanceof UpdateableState) {
+                final String[] sTarget = target.toString().split("/");
+                if (sTarget[sTarget.length - 2].toLowerCase().contentEquals("node")) {
+                    ((UpdateableState) target).flush();
+                }
             }
             // todo take mapping policies for relationships into account
             persistentEntity.doWithAssociations(new AssociationHandler<Neo4jPersistentProperty>() {
